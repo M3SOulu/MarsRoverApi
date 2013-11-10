00d1c84b40acec93be70b55bb41531f4239aaa9f
slacko <ancestrall@gmail.com>
2013-11-10 19:20:12 +0800

    first commit

---
+++ src/training/tdd/day1/day1/test/TestRover.java
@@ -0,0 +1,14 @@
+package training.tdd.day1.day1.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRover {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
