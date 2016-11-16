11af10e2541af09f879b1e3cd770c0de39fc9f49
somename <someemail>
2016-11-16 17:23:52 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRoverException.java
+++ src/tdd/training/session1/MarsRoverException.java
@@ -7,7 +7,7 @@ public class MarsRoverException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public MarsRoverException(Exception e){
-		System.err.println(e.getMessage());
+	public MarsRoverException(String e){
+		System.err.println(e);
 	}
 }
