cf9b2140f2d13d0374708e13ea5b06d8e5a20252
somename <someemail>
2016-11-16 17:17:12 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,8 +1,10 @@
 package tdd.training.session1;
 
 public class MarsRover {
-	
+
 	Rover A;
+	int global_x;
+	int global_y;
 
 	public MarsRover(int x, int y, String obstacles) {
 		/*
@@ -14,6 +16,8 @@ public class MarsRover {
 		 * //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
 		 */
 		A = new Rover();
+		global_x = x;
+		global_y = y;
 	}
 
 	public String executeCommand(String command) {
@@ -29,63 +33,161 @@ public class MarsRover {
 		 * pointing to (N,S,W,E). The return string should also contain a list
 		 * of coordinates of the encountered obstacles. No white spaces.
 		 */
-		
+
 		for (int i = 0; i <= command.length(); i++) {
 			char cmd = command.charAt(i);
 			int moment = 0;
-			int moment1 = 0;
-			
-			if(cmd == 'f'){
+
+			if (cmd == 'f') {
 				moment = A.getPos_y() + 1;
 				A.setPos_y(moment);
 				System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
-			} else if(cmd  == 'b'){
+			} else if (cmd == 'b') {
 				moment = A.getPos_y() - 1;
 				A.setPos_y(moment);
 				System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
-			} else if(cmd == 'r'){
+			} else if (cmd == 'r') {
 				System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
-				while(cmd != 'r' || cmd != 'l'){
-					
+				i++;
+				char cmd1 = command.charAt(i);
+				while (cmd1 != 'r' || cmd1 != 'l') {
+					if (cmd1 == 'f') {
+						moment = A.getPos_x() + 1;
+						A.setPos_x(moment);
+						System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+						i++;
+						cmd1 = command.charAt(i);
+					} else if (cmd1 == 'b') {
+						moment = A.getPos_x() - 1;
+						A.setPos_x(moment);
+						System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+						i++;
+						cmd1 = command.charAt(i);
+					}
+				}
+				if (cmd1 == 'r') {
+					System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+					i++;
+					char cmd2 = command.charAt(i);
+					while (cmd2 != 'r' || cmd2 != 'l') {
+						if (cmd2 == 'f') {
+							moment = A.getPos_y() - 1;
+							A.setPos_y(moment);
+							System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+							i++;
+							cmd2 = command.charAt(i);
+						} else if (cmd2 == 'b') {
+							moment = A.getPos_y() + 1;
+							A.setPos_y(moment);
+							System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+							i++;
+							cmd2 = command.charAt(i);
+						}
+					}
+					if (cmd2 == 'r') {
+						System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+						i++;
+						char cmd3 = command.charAt(i);
+						while (cmd3 != 'r' || cmd3 != 'l') {
+							if (cmd3 == 'f') {
+								moment = A.getPos_x() - 1;
+								A.setPos_x(moment);
+								System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+								i++;
+								cmd3 = command.charAt(i);
+							} else if (cmd3 == 'b') {
+								moment = A.getPos_x() + 1;
+								A.setPos_x(moment);
+								System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+								i++;
+								cmd3 = command.charAt(i);
+							}
+						}
+					}
+				}
+			} else if (cmd == 'l') {
+				System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+				i++;
+				char cmd1 = command.charAt(i);
+				while (cmd1 != 'r' || cmd1 != 'l') {
+					if (cmd1 == 'f') {
+						moment = A.getPos_x() - 1;
+						A.setPos_x(moment);
+						System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+						i++;
+						cmd1 = command.charAt(i);
+					} else if (cmd1 == 'b') {
+						moment = A.getPos_x() + 1;
+						A.setPos_x(moment);
+						System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+						i++;
+						cmd1 = command.charAt(i);
+					}
+				}
+				if (cmd1 == 'l') {
+					System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+					i++;
+					char cmd2 = command.charAt(i);
+					while (cmd2 != 'r' || cmd2 != 'l') {
+						if (cmd2 == 'f') {
+							moment = A.getPos_y() - 1;
+							A.setPos_y(moment);
+							System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+							i++;
+							cmd2 = command.charAt(i);
+						} else if (cmd2 == 'b') {
+							moment = A.getPos_y() + 1;
+							A.setPos_y(moment);
+							System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+							i++;
+							cmd2 = command.charAt(i);
+						}
+					}
+					if (cmd2 == 'l') {
+						System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+						i++;
+						char cmd3 = command.charAt(i);
+						while (cmd3 != 'r' || cmd3 != 'l') {
+							if (cmd3 == 'f') {
+								moment = A.getPos_x() + 1;
+								A.setPos_x(moment);
+								System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+								i++;
+								cmd3 = command.charAt(i);
+							} else if (cmd3 == 'b') {
+								moment = A.getPos_x() - 1;
+								A.setPos_x(moment);
+								System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
+								i++;
+								cmd3 = command.charAt(i);
+							}
+						}
+					}
 				}
 			}
-			/*int moment = 0;
-			int moment1 = 0;
-			
-			switch (command.charAt(i)) {
-			case 'f':
-				moment = A.getPos_y() + 1;
-				A.setPos_y(moment);
-				System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
-				break;
-
-			case 'b':
-				moment = A.getPos_y() - 1;
-				A.setPos_y(moment);
-				System.out.print("(" + A.getPos_x() + "," + A.getPos_y() + ")");
-				break;
-
-			case 'l':
-				moment = A.getPos_x();
-				moment1 = A.getPos_y();
-				
-				A.setPos_x(moment1);
-				A.setPos_y(moment);
-				System.out.print("(" + A.getPos_y() + "," + A.getPos_x() + ")");
-				break;
-
-			case 'r':
-				moment = A.getPos_x();
-				moment1 = A.getPos_y();
-				
-				A.setPos_x(moment1);
-				A.setPos_y(moment);
-				System.out.print("(" + A.getPos_y() + "," + A.getPos_x() + ")");
-				break;
-			}*/
-
 		}
-		
+
 		return null;
 	}
 }
+
+/*
+ * int moment = 0; int moment1 = 0;
+ * 
+ * switch (command.charAt(i)) { case 'f': moment = A.getPos_y() + 1;
+ * A.setPos_y(moment); System.out.print("(" + A.getPos_x() + "," + A.getPos_y()
+ * + ")"); break;
+ * 
+ * case 'b': moment = A.getPos_y() - 1; A.setPos_y(moment); System.out.print("("
+ * + A.getPos_x() + "," + A.getPos_y() + ")"); break;
+ * 
+ * case 'l': moment = A.getPos_x(); moment1 = A.getPos_y();
+ * 
+ * A.setPos_x(moment1); A.setPos_y(moment); System.out.print("(" + A.getPos_y()
+ * + "," + A.getPos_x() + ")"); break;
+ * 
+ * case 'r': moment = A.getPos_x(); moment1 = A.getPos_y();
+ * 
+ * A.setPos_x(moment1); A.setPos_y(moment); System.out.print("(" + A.getPos_y()
+ * + "," + A.getPos_x() + ")"); break; }
+ */
