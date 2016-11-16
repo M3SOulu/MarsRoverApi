3fa2118295cfe685d728af874c014a5cba7b0b2f
somename <someemail>
2016-11-16 17:37:02 +0100

    besouro automatic message

--- src/tdd/training/session1/Main.java
+++ src/tdd/training/session1/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) throws MarsRoverException {
 		// TODO Auto-generated method stub
-		MarsRover A = new MarsRover(100,100,"(0,7)");
-		A.executeCommand("ffrb");
+		MarsRover C = new MarsRover(100,100,"(0,7)");
+		C.executeCommand("ffrb");
 	}
 
 }
