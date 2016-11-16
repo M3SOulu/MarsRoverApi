679608857e97c36f545262c6b5624ae61e45ad56
somename <someemail>
2016-11-16 15:44:41 +0100

    besouro automatic message

--- src/tdd/training/session1/Navigation.java
+++ src/tdd/training/session1/Navigation.java
@@ -1,8 +1,12 @@
 package tdd.training.session1;
 
 public class Navigation {
-	public Navigation(){
-		
+	private int x;
+	private int y;
+	
+	public Navigation(int x, int y, String obstacle){
+		this.x = x;
+		this.y = y;
 	}
 
 }
