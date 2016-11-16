97cec7faebcd1a69a1638cdb74777cd7690639d1
somename <someemail>
2016-11-16 15:52:22 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -20,13 +20,13 @@ public class MarsRover {
 			if (Utils.countOccurencesOf(obstacles, "(") != Utils.countOccurencesOf(obstacles, ")") ||
 				Utils.countOccurencesOf(obstacles, "(") != Utils.countOccurencesOf(obstacles, ",")){
 				throw new MarsRoverException();
+				System.out.println("bad");
 			}
 			String[] values = obstacles.replace(")", "").split("(");
 			for (String value : values){
 				String xPos = value.split(",")[0];
 				String yPos = value.split(",")[1];
 				if (!Utils.isInteger(xPos) || !Utils.isInteger(yPos)){
-					System.out.println("bad");
 					throw new MarsRoverException();
 				}
 				this.obstacles.add(new Position(Integer.parseInt(xPos), Integer.parseInt(yPos)));
