37f58ff2899f807c99a708dcd3dfa493b5847e18
somename <someemail>
2016-11-16 17:21:39 +0100

    besouro automatic message

--- src/tdd/training/session1/Obstacle.java
+++ src/tdd/training/session1/Obstacle.java
@@ -3,7 +3,8 @@ package tdd.training.session1;
 public class Obstacle extends Coordinates {
 
 	public Obstacle(int x_coordinate, int y_coordinate) {
-		super(x_coordinate, y_coordinate);
+		super(x_coordinate, y_coordinate).toString();
+		
 		
 	}
 
