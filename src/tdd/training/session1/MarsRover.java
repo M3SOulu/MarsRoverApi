4de4707833a2765d79c20235c8a96f19aaffb947
somename <someemail>
2016-11-16 18:13:44 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -158,7 +158,7 @@ public class MarsRover {
 	public static void main(String[] args) throws MarsRoverException{
 
 		MarsRover rover = new MarsRover(3, 3, "(2,2)");
-		String test = rover.executeCommand("ffff");
+		String test = rover.executeCommand("ff");
 		System.out.println(test);
 		// Object array[][] = new Object[3][4];
 		// System.out.println(array[0].length);
