028ef76764697827398b91930d2e4216fc26952b
somename <someemail>
2016-11-16 18:16:22 +0100

    besouro automatic message

--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -13,6 +13,7 @@ public class MarsRoverTest {
 	@Test
 	public void executeCommand() {
 		assertEquals("(1,2,E)",marsRover.executeCommand("ffrf"));
+		assertEquals("(1,1,E)",marsRover.executeCommand("frf"));
 	}
 
 	
