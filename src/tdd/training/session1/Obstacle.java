faaafd2a59eee7943d2f4b21ff8999f0168a36a7
somename <someemail>
2016-11-16 17:14:01 +0100

    besouro automatic message

--- src/tdd/training/session1/Obstacle.java
+++ src/tdd/training/session1/Obstacle.java
@@ -3,4 +3,16 @@ package tdd.training.session1;
 public class Obstacle {
 	private int x;
 	private int y;
+	
+	public Obstacle(){
+		
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setX(int y){
+		this.y = y;
+	}
 }
