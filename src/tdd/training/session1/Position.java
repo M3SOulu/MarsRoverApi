96bc3d4527270d6d1a2186eb96096ab177b71ac8
somename <someemail>
2016-11-16 15:37:22 +0100

    besouro automatic message

--- src/tdd/training/session1/Position.java
+++ src/tdd/training/session1/Position.java
@@ -3,9 +3,11 @@ package tdd.training.session1;
 public class Position {
 	int x = 0;
 	int y = 0;
+	
 	public Position(){
 		this(0, 0);
 	}
+	
 	public Position(int x, int y){
 		this.x = x;
 		this.y = y;
