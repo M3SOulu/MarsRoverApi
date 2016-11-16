b60df557f6d2371b456738e701698ccc5b1211f7
studente <studente@turing-13.boole.local>
2016-11-16 18:22:09 +0100

    Test 16/11/2016

--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -7,14 +7,17 @@ import org.junit.Test;
 
 import tdd.training.session1.MarsRover;
 import tdd.training.session1.MarsRoverException;
+import tdd.training.session1.Rover;
 
 public class MarsRoverTest {
 
 	MarsRover rover;
+	Rover d;
 	
 	@Before
 	public void dichiaro(){
 		rover = new MarsRover(100,100,"(33,40)");
+		d = new Rover();
 	}
 	
 	@Test
@@ -23,9 +26,16 @@ public class MarsRoverTest {
 		Assert.assertNotEquals("(0,1)(0,2)(0,2)", rover.executeCommand("f"));
 	}
 	
+	@Test
+	public void testRover() {
+		Assert.assertEquals(0, d.getPos_x());
+		Assert.assertNotEquals(1, d.getPos_x());
+	}
+	
 	@After
 	public void cancello(){
 		rover = null;
+		d = null;
 	}
 
 }
