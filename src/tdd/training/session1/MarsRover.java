0b9be77287f9b0c3873220cef21e68b26969fbbd
somename <someemail>
2016-11-16 15:38:29 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -18,34 +18,24 @@ public class MarsRover {
 		direction = 0;
 	}
 	
-	
-	
 	public int getCoordX() {
 		return coordX;
 	}
-
-
 
 	public void setCoordX(int coordX) {
 		this.coordX = coordX;
 	}
 
-
-
 	public int getCoordY() {
 		return coordY;
 	}
-
-
 
 	public void setCoordY(int coordY) {
 		this.coordY = coordY;
 	}
 
-
-
 	public String getRealDirection() {
-		return position.get(direction);
+		return cardinalPosition.get(direction);
 	}
 
 
