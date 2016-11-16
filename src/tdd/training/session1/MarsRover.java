81607f683959fb404e082383cbd98b55a3edaaee
somename <someemail>
2016-11-16 17:29:04 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -112,6 +112,27 @@ public class MarsRover {
 		}
 	}
 
+	public void moveBackward(){
+		switch(direction.getValue()){
+		case 'N':
+			if(yPos<ySize-1) yPos++;
+			else yPos = 0;			
+			break;
+		case 'S':
+			if(yPos>0) yPos--;
+			else yPos = ySize-1;
+			break;
+		case 'E':
+			if(xPos<xSize-1) xPos++;
+			else xPos = 0;
+			break;
+		case 'W':
+			if(xPos>0) xPos--;
+			else xPos = xSize-1;
+			break;
+		}
+	}
+	
 	
 	public void moveLeft(){
 		
