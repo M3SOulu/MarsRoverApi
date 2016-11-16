0aa9ca30ade3eedd712d8f5216584ae8e0669ced
somename <someemail>
2016-11-16 15:37:47 +0100

    besouro automatic message

--- src/tdd/training/session1/Obstacle.java
+++ src/tdd/training/session1/Obstacle.java
@@ -5,5 +5,19 @@ package tdd.training.session1;
  */
   public class Obstacle {
  
-
+	  private int x;
+	  private int y;
+	  
+	  public Obstacle(int x, int y){
+		  
+		  this.x=x;
+		  this.y=y;
+				  
+	  }
+	  
+	  @Override
+	  public String toString(){
+		  
+		  return new String("("+x+","+y+")");
+	  }
 }
