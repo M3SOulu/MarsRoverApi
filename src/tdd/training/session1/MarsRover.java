b6f4489284768d95760ec1b8b409563d7e07a542
somename <someemail>
2016-11-16 17:21:18 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,6 +1,14 @@
 package tdd.training.session1;
 
 public class MarsRover {
+	
+	public static final Integer N = 1;
+	public static final Integer E = 2;
+	public static final Integer S = 3;
+	public static final Integer W = 4;
+	Integer x = 0;
+	Integer y = 0;
+	Integer facing = N;
 	public MarsRover(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces. 
@@ -21,6 +29,47 @@ public class MarsRover {
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
 		
+		for (int idx = 0; idx < command.length(); idx++) {
+			executeCommand(command.charAt(idx));
+		}
+		
 		return null;
+	}
+	
+
+	private void executeCommand(Character command) {
+		if (command.equals('L')) {
+			turnLeft();
+		} else if (command.equals('R')) {
+			turnRight();
+		} else if (command.equals('M')) {
+			move();
+		} else {
+			throw new IllegalArgumentException(
+					"Speak in Mars language, please!");
+		}
+	}
+	public void move() {
+		if (facing == N) {
+			this.y++  ;
+		} else if (facing == E) {
+			this.x++  ;
+		} else if (facing == S) {
+			this.y--;
+		} else if (facing == W) {
+			this.x--;
+		}
+	}
+	public void turnLeft() {
+		facing = (facing - 1) < N ? W : facing - 1;
+	}
+	public void turnRight() {
+		facing = (facing +  1) > W ? N : facing + 1;
+	}
+	
+	public void setPosition(Integer x, Integer y, Integer facing) {
+		this.x = x;
+		this.y = y;
+		this.facing = facing;
 	}
 }
--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import tdd.training.session1.MarsRover;
+
 public class MarsRoverTest {
+
+	private static final Integer N = 1;
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		MarsRover rover = new MarsRover(0, 0, "");
+		rover.setPosition(0, 0, N);
+		rover.turnRight();
+		rover.move();
+		rover.toString();
+		
+		
+		
 	}
 
 }
