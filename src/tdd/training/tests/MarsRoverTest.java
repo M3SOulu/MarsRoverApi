975fc339599eb7efd107e014cc865f2bd8e0edd6
somename <someemail>
2016-11-16 17:56:45 +0100

    besouro automatic message

--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -3,13 +3,23 @@ package tdd.training.tests;
 import static org.junit.Assert.*;
 import java.util.*;
 
-import org.junit.Test;
+import org.junit.*;
+
+import tdd.training.session1.Coordinates;
+import tdd.training.session1.Obstacle;
 
 public class MarsRoverTest {
 
+	private Coordinates coordinates;
+	private Obstacle obs;
+	
+ 
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	public void receivSingleCommandShouldMoveForwardIfCommandIsF {
+		
 	}
 
 }
