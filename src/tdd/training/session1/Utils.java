1c5008ad179e2f60b8748dbf4e6f031520e9a175
somename <someemail>
2016-11-16 15:45:32 +0100

    besouro automatic message

--- src/tdd/training/session1/Utils.java
+++ src/tdd/training/session1/Utils.java
@@ -15,4 +15,16 @@ public class Utils {
 		String str = "(" + x + "," + y + "," + dir + ")";
 		return str;
 	}
+	
+	public static boolean isInteger(String s, int radix) {
+	    if(s.isEmpty()) return false;
+	    for(int i = 0; i < s.length(); i++) {
+	        if(i == 0 && s.charAt(i) == '-') {
+	            if(s.length() == 1) return false;
+	            else continue;
+	        }
+	        if(Character.digit(s.charAt(i),radix) < 0) return false;
+	    }
+	    return true;
+	}
 }
