e800ab526b5b592df9c311e642dfe65eb11f9ef8
somename <someemail>
2016-11-16 18:24:29 +0100

    besouro automatic message

--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -13,6 +13,6 @@ public class MarsRoverTest {
 	
 	@Test(expected = MarsRoverException.class)
 	public void executeCommand() throws MarsRoverException{
-		assertEquals("",marsRover.executeCommand("q"));
+		assertEquals(nul,marsRover.executeCommand("q"));
 	}
 }
