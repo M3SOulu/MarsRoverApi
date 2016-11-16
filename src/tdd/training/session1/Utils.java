2177d6567242b5853698f0b13c42438d1d126264
somename <someemail>
2016-11-16 15:32:35 +0100

    besouro automatic message

--- src/tdd/training/session1/Utils.java
+++ src/tdd/training/session1/Utils.java
@@ -1,7 +1,7 @@
 package tdd.training.session1;
 
 public class Utils {
-	public int countOccurencesOf(String str, String chars){
+	public static int countOccurencesOf(String str, String chars){
 		int count = str.length() - str.replace(chars, "").length();
 		return count;
 	}
