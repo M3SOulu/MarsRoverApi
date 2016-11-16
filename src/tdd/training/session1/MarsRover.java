bc938bfa92e6c20cfab120156e9105818f6eb9b4
somename <someemail>
2016-11-16 15:36:25 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,13 +1,23 @@
 package tdd.training.session1;
 
 public class MarsRover {
+	
+	
+	private Object planet[][];
+	
 	public MarsRover(int x, int y, String obstacles){
+		
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces. 
 	 *  
 		Example use:
 		MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
+		
+		planet = new Object[x][y];
+		
+		
+		
 	}
 	
 	public String executeCommand(String command){
