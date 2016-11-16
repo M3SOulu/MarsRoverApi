7919a075b5af38eb760e2baa69ac48396012811e
somename <someemail>
2016-11-16 15:51:49 +0100

    besouro automatic message

--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -4,13 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import tdd.training.session1.Obstacle;
+
 public class MarsRoverTest {
 	
 	
 	
 	@Test
-	public void Obstacle() {
-		fail("Not yet implemented");
+	public void ObstacleTest() {
+		Obstacle ob1= new Obstacle(1,2);
 	}
 
 }
