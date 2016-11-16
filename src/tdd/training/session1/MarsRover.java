b1b1a6616c9f21430b5b1d33b3c1d354f1689d28
somename <someemail>
2016-11-16 15:56:06 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -21,7 +21,7 @@ public class MarsRover {
 				Utils.countOccurencesOf(obstacles, "(") != Utils.countOccurencesOf(obstacles, ",")){
 				throw new MarsRoverException();
 			}
-			System.out.println("ok0");
+			System.out.println("ok-1");
 			String[] values = obstacles.replace(")", "").split("(");
 			for (String value : values){
 				String xPos = value.split(",")[0];
