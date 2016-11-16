636d2d93bd8562de0c454331d0b46eef5cfaf707
somename <someemail>
2016-11-16 15:22:19 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -8,6 +8,8 @@ public class MarsRover {
 		Example use:
 		MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
+		
+		
 	}
 	
 	public String executeCommand(String command){
