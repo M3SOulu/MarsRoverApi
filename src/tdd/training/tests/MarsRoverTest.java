80f0a903d1bfb8e4b9e60fdb0c47caebdcaf70f7
somename <someemail>
2016-11-16 16:59:13 +0100

    besouro automatic message

--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -2,9 +2,19 @@ package tdd.training.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import tdd.training.session1.MarsRover;
+
 public class MarsRoverTest {
+	
+	MarsRover sonic;
+	
+	@Before
+	public void initialize(){
+		sonic = new MarsRover(10,10,"?(")
+	}
 
 	@Test
 	public void test() {
