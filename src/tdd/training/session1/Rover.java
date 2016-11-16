46a107efa0fbc38382abdef6bb7e77f7885c488e
somename <someemail>
2016-11-16 15:45:15 +0100

    besouro automatic message

---
+++ src/tdd/training/session1/Rover.java
@@ -0,0 +1,29 @@
+package tdd.training.session1;
+
+public class Rover {
+	
+	private int pos_x;
+	private int pos_y;
+	
+	public Rover(){
+		pos_x = 0;
+		pos_y = 0;
+	}
+
+	public int getPos_x() {
+		return pos_x;
+	}
+
+	public void setPos_x(int pos_x) {
+		this.pos_x = pos_x;
+	}
+
+	public int getPos_y() {
+		return pos_y;
+	}
+
+	public void setPos_y(int pos_y) {
+		this.pos_y = pos_y;
+	}
+	
+}
