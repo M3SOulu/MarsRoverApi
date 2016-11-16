ce8ef9e8ca0254c56064ed1d83cb2e93375cde26
somename <someemail>
2016-11-16 15:45:45 +0100

    besouro automatic message

--- src/tdd/training/session1/Utils.java
+++ src/tdd/training/session1/Utils.java
@@ -16,6 +16,10 @@ public class Utils {
 		return str;
 	}
 	
+	public static boolean isInteger(String s) {
+	    return isInteger(s,10);
+	}
+	
 	public static boolean isInteger(String s, int radix) {
 	    if(s.isEmpty()) return false;
 	    for(int i = 0; i < s.length(); i++) {
