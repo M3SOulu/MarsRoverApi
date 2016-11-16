29c8f7853e9ca64cb1dc14cd75a165ccf18960cc
somename <someemail>
2016-11-16 15:41:21 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,15 +1,19 @@
 package tdd.training.session1;
 
 public class MarsRover {
-	public MarsRover(int x, int y, String obstacles){
-	/*	x and y represent the size of the grid.
-	 *  Obstacles is a String formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces. 
-	 *  
-		Example use:
-		MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
-	 */
+
+	public MarsRover(int x, int y, String obstacles) {
+		/*
+		 * x and y represent the size of the grid. Obstacles is a String
+		 * formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no
+		 * white spaces.
+		 * 
+		 * Example use: MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")
+		 * //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
+		 */
+
 	}
-	
+
 	public String executeCommand(String command){
 		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
@@ -20,6 +24,26 @@ public class MarsRover {
 		 * Where x and y are the final coordinates, facing is the current direction the rover is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		for(int i = 0; i<=command.length(); i++){ 
+			
+				switch(command.charAt(i)) { 
+					case 'f': 
+			 			
+		 				break; 
+		 			
+					case 'b':
+						
+						break;
+						
+					case 'l':
+						
+						break;
+						
+					case 'r':
+						
+						break;
+				}
+
 		
 		return null;
 	}
