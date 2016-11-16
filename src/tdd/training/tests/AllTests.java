5c23299cc827eb8be62d89bd8fef88f94e8a8dcd
somename <someemail>
2016-11-16 16:18:21 +0100

    besouro automatic message

---
+++ src/tdd/training/tests/AllTests.java
@@ -0,0 +1,11 @@
+package tdd.training.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MarsRoverTest.class, NavigationTest.class })
+public class AllTests {
+
+}
---
+++ src/tdd/training/tests/NavigationTest.java
@@ -0,0 +1,14 @@
+package tdd.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NavigationTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
