1601d39c5906337a82314ef9d131a8905884cdd7
somename <someemail>
2016-11-16 16:02:41 +0100

    besouro automatic message

---
+++ src/tdd/training/session1/Coordinates.java
@@ -0,0 +1,40 @@
+package tdd.training.session1;
+
+public class Coordinates {
+
+	public Coordinates(int x_coordinate, int y_coordinate) {
+		
+		this.x_coordinate = x_coordinate;
+		this.y_coordinate = y_coordinate;
+	}
+	/**
+	 * @return the x_coordinate
+	 */
+	public int getX_coordinate() {
+		return x_coordinate;
+	}
+	/**
+	 * @param x_coordinate the x_coordinate to set
+	 */
+	public void setX_coordinate(int x_coordinate) {
+		this.x_coordinate = x_coordinate;
+	}
+	/**
+	 * @return the y_coordinate
+	 */
+	public int getY_coordinate() {
+		return y_coordinate;
+	}
+	/**
+	 * @param y_coordinate the y_coordinate to set
+	 */
+	public void setY_coordinate(int y_coordinate) {
+		this.y_coordinate = y_coordinate;
+	}
+	int x_coordinate;
+	int y_coordinate;
+	
+	public Coordinates getCoordinates(){
+		return (getX_coordinate(), getY_coordinate());
+	}
+}
