b969e39511ba578d6d7f39a2ebff353b4e31b5aa
somename <someemail>
2016-11-16 16:33:49 +0100

    besouro automatic message

--- src/tdd/training/tests/NavigationTest.java
+++ src/tdd/training/tests/NavigationTest.java
@@ -15,7 +15,7 @@ public class NavigationTest {
 	@Test (expected = MarsRoverException.class)
 	public void testGeneraOstacoli() {
 		assertEquals(2, navTest.getObstacles().size());
-		assertEquals("12", navTest.getObstacles().get(1));
+		assertEquals("12", navTest.getObstacles().get(0));
 	}
 
 }
