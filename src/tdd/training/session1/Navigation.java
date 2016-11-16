0d747ebaae293fec44e0b23811d25669dd1eab8a
somename <someemail>
2016-11-16 16:57:23 +0100

    besouro automatic message

--- src/tdd/training/session1/Navigation.java
+++ src/tdd/training/session1/Navigation.java
@@ -19,9 +19,9 @@ public class Navigation {
 		for (int i = 0; i < ob.length() ; i++){
 			
 			if(ob.charAt(i) != '?' && ob.charAt(i) != '(' && ob.charAt(i) != ','){
+				
 				if(ob.charAt(i) == ')'){
-					obstacles.add(currentObstacle);
-					
+					obstacles.add(currentObstacle);					
 					currentObstacle = "";
 				}
 				else{
