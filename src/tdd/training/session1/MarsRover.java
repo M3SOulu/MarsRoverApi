ec92f211f34570554902b7bc8d6b6a06b98ecd1b
somename <someemail>
2016-11-16 16:19:56 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,7 +1,7 @@
 package tdd.training.session1;
 
 public class MarsRover {
-	final int GRID = 3;
+
 	public MarsRover(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces. 
@@ -9,20 +9,17 @@ public class MarsRover {
 		Example use:
 		MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
-	String North = null;
-	int[][] position1 = null;
-	String position2 = North;
-	String[] obs_x = null;
-	String[] obs_y = null;
-	String[][] obs = null;
-	for(int i = 0; i<GRID;i++){
-		obs[i] = obs_x;
-		for(int j=0 ; j<GRID ; j++){
-			obs[j] = obs_y;
+	
+	String obs_x = null;
+	String obs_y = null;
+	
+	for(int i = 0; i< x;i++){
+		obstacles = obs_x;
+		for(int j=0 ; j< y ; j++){
+			obstacles = obs_y;
 		}
 	}
-	
-	MarsRover rover = new MarsRover(GRID,GRID,"(obs_x,obs_y)");
+	MarsRover rover = new MarsRover(x,x,"(obs_x,obs_y)");
 	
 	
 }
@@ -37,6 +34,8 @@ public class MarsRover {
 		 * Where x and y are the final coordinates, facing is the current direction the rover is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		
+		
 		
 		return null;
 	}
