cd2c97d8632d1b62aaf19c56690dc32586e0077d
somename <someemail>
2016-11-16 15:53:48 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,26 +1,55 @@
 package tdd.training.session1;
 
 public class MarsRover {
-	public MarsRover(int x, int y, String obstacles){
-	/*	x and y represent the size of the grid.
-	 *  Obstacles is a String formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces. 
-	 *  
-		Example use:
-		MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
-	 */
+	private int[][] grid;
+
+	public MarsRover(int x, int y, String obstacles) {
+		/*
+		 * x and y represent the size of the grid. Obstacles is a String
+		 * formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no
+		 * white spaces.
+		 * 
+		 * Example use: MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")
+		 * //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
+		 */
+
+		grid = new int[x][y];
+		setObstacles(obstacles);
+
+	}
+
+	public String executeCommand(String command) {
+
+		/*
+		 * The command string is composed of "f" (forward), "b" (backward), "l"
+		 * (left) and "r" (right) Example: The rover is on a 100x100 grid at
+		 * location (0, 0) and facing NORTH. The rover is given the commands
+		 * "ffrff" and should end up at (2, 2) facing East.
+		 * 
+		 * The return string is in the format:
+		 * ?(x,y,facing)(o1_x,o1_y)(o2_x,o2_y)?..(on_x,on_y)? Where x and y are
+		 * the final coordinates, facing is the current direction the rover is
+		 * pointing to (N,S,W,E). The return string should also contain a list
+		 * of coordinates of the encountered obstacles. No white spaces.
+		 */
+
+		return null;
+	}
+
+	private void setObstacles(String obstacles) {
+
+		char[] obstaclesA = obstacles.toCharArray();
+		for (int i = 0; i < obstacles.length(); i++) {
+			if (obstaclesA[i] == '(') {
+				grid[Integer.valueOf(obstaclesA[i + 1])][Integer.valueOf(obstaclesA[i + 3])] = -1;
+
+			}
+		}
+
 	}
 	
-	public String executeCommand(String command){
+	public int[][] getGrid(){
+		return grid;
 		
-		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
-		 * Example: 
-		 * The rover is on a 100x100 grid at location (0, 0) and facing NORTH. The rover is given the commands "ffrff" and should end up at (2, 2) facing East.
-		 
-		 * The return string is in the format: ?(x,y,facing)(o1_x,o1_y)(o2_x,o2_y)?..(on_x,on_y)?  
-		 * Where x and y are the final coordinates, facing is the current direction the rover is pointing to (N,S,W,E).
-		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
-		 */
-		
-		return null;
 	}
 }
