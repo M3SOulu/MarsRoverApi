9cc0be00eaa405332808bccfd57dfd14fabe39e9
somename <someemail>
2016-11-16 16:22:04 +0100

    besouro automatic message

--- src/tdd/training/session1/MarsRover.java
+++ src/tdd/training/session1/MarsRover.java
@@ -1,6 +1,11 @@
 package tdd.training.session1;
 
+import java.util.Arrays;
+
+import javax.management.monitor.StringMonitor;
+
 public class MarsRover {
+	Boolean planet[][];
 	public MarsRover(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: ?(o1_x,o1_y)(o2_x,o2_y)...(on_x,on_y)? with no white spaces. 
@@ -8,6 +13,10 @@ public class MarsRover {
 		Example use:
 		MarsRover rover = new MarsRover(100,100,"?(5,5)(7,8)?")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
+		planet = new Boolean[x][y];
+		Arrays.fill(planet, false);
+		
+
 	}
 	
 	public String executeCommand(String command){
@@ -22,5 +31,17 @@ public class MarsRover {
 		 */
 		
 		return null;
+	}
+	
+	private void positionObstacles(String obstacles, Boolean planet [][]){
+		
+		String temp[] = obstacles.split(")");
+		temp[1].replace('(', ' ');
+		temp[1] = temp[1].trim();
+		String locationTemp[] = temp[1].split(",");
+		
+		planet[Integer.parseInt(locationTemp[1])][Integer.parseInt(locationTemp[2])] = false;
+		
+		
 	}
 }
