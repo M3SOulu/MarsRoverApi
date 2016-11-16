37e3a38bf0964c449d0ea9d5cba50a6e9ede213a
somename <someemail>
2016-11-16 17:16:04 +0100

    besouro automatic message

--- src/tdd/training/tests/MarsRoverTest.java
+++ src/tdd/training/tests/MarsRoverTest.java
@@ -83,4 +83,8 @@ public class MarsRoverTest {
 		assertEquals(Direction.NORTH, MarsRover.moveLeft(Direction.EAST));
 	}
 	
+	@Test
+	public void testMoveForward(){
+		marsRover.moveForward();
+	}
 }
