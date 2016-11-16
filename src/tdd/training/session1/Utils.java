0f0a306c15c57b28d62f2167c5549b485a6de317
somename <someemail>
2016-11-16 16:06:43 +0100

    besouro automatic message

--- src/tdd/training/session1/Utils.java
+++ src/tdd/training/session1/Utils.java
@@ -1,6 +1,7 @@
 package tdd.training.session1;
 
 public class Utils {
+	public String acceptedInput = "fblr";
 	public static int countOccurencesOf(String str, String chars){
 		int count = str.length() - str.replace(chars, "").length();
 		return count;
